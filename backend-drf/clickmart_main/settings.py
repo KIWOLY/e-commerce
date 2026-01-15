4e5fa32badb5f0c060cb870788ecbb4d03a7405f
KIWOLY <ikiwoly@gmail.com>
2026-01-15 14:27:19 +0300

    change workflows

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -138,9 +138,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS = [
-    'clickmart_main/static'
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
