16ecbfb4ab3c409df0b8c64441609fe1df069b64
KIWOLY <ikiwoly@gmail.com>
2026-01-27 11:07:56 +0300

    add CSRF v2

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -181,4 +181,4 @@ CORS_ALLOWED_ORIGINS = [
     "https://cognitech.tlms.live"
 ]
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
